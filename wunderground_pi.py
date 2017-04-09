4363553ca20784a80cbfde1c07c0e9764bc2c8bb
Alex Gendreau <alex.gendreau24@gmail.com>
2017-04-09 21:59:56 +0000

    updated to work on pi

--- wunderground_pi.py
+++ wunderground_pi.py
@@ -27,7 +27,7 @@ import json
 
 import argparse
 import time
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 
 import requests
@@ -36,7 +36,7 @@ import requests
 red = 16
 green = 20
 blue = 21
-'''
+
 # GPIO setup.
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -52,24 +52,31 @@ BLUE = GPIO.PWM(blue, 100)
 RED.start(0)
 GREEN.start(0)
 BLUE.start(0)
-'''
+
 
 def learn(features,target):
 
 	X = np.matrix(features)
-	print X
+	print(X)
 	Y = np.matrix(target)
-	print Y
+	print(Y)
 	clf = linear_model.LinearRegression()
 	clf = clf.fit(X,Y)
 	return clf
 
 # Set a color by giving R, G, and B values of 0-255.
-def setColor(red,green,blue):
+def setColor(r,g,b):
     # Convert 0-255 range to 0-100.
+    red = (r/255.0) * 100
+    blue = (b/255.0)*100
+    green = (g/255.0)*100
+    print(red)
+    print(green)
+    print(blue) 
     RED.ChangeDutyCycle(red)
     GREEN.ChangeDutyCycle(green)
     BLUE.ChangeDutyCycle(blue)
+    time.sleep(5)
 
 def give_me_data(request):
 	v = request.json()
@@ -77,13 +84,13 @@ def give_me_data(request):
 	humid = float(v['history']['observations'][0]['hum'])
 	pressure = float(v['history']['observations'][0]['pressurei'])
 	wind = float(v['history']['observations'][0]['wspdi'])
-	print "temp: " + str(temp)
-	print "wind: " + str(wind)
-	print "pressure: " + str(pressure)
-	print "humidity: " + str(humid)
-	red = int(raw_input("Give me red value\n"))
-	green = int(raw_input("Give me green value\n"))
-	blue = int(raw_input("Give me blue value\n"))
+	print("temp: " + str(temp))
+	print("wind: " + str(wind))
+	print("pressure: " + str(pressure))
+	print("humidity: " + str(humid))
+	red = int(input("Give me red value\n"))
+	green = int(input("Give me green value\n"))
+	blue = int(input("Give me blue value\n"))
 	return ([temp,wind,pressure,humid],[red,green,blue])
 
 def generate_data(city,state):
@@ -119,20 +126,20 @@ def predict(reg_model,city,state):
 	pressure = float(values["current_observation"]["pressure_in"])
 	humid = float(values["current_observation"]["relative_humidity"].strip('%'))
 	closest = reg_model.predict(np.matrix([[temp,wind,pressure,humid]]))
-	red = int(np.round(closest[0][0]))
-	green = int(np.round(closest[0][1]))
-	blue = int(np.round(closest[0][2]))
+	red = abs(int(np.round(closest[0][0])))%255
+	green = abs(int(np.round(closest[0][1])))%255
+	blue = abs(int(np.round(closest[0][2])))%255
 	print(red,green,blue)
-	#setColor(red,green,blue)
+	setColor(red,green,blue)
 
 
 if __name__ == "__main__":
-	city = raw_input("Give me a city\n")
-	state = raw_input("Give me the state\n")
+	city = input("Give me a city\n")
+	state = input("Give me the state\n")
 	features,target = generate_data(city,state)
 	reg_model = learn(features,target)
 	predict(reg_model,city,state) 
 	
-	'''
+	
 	GPIO.cleanup()
-	'''
+	
